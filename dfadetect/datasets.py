1e9e7e17bc5331a2be3105f6019655a0fec548a3
Hanan Haider <137984110+hanan-haider@users.noreply.github.com>
2026-04-20 11:38:03 +0500

    .

--- dfadetect/datasets.py
+++ dfadetect/datasets.py
@@ -8,9 +8,9 @@ import torch
 import torchaudio
 from torchaudio.functional import apply_codec
 
-from dfadetect.lfcc import LFCC
-from dfadetect.utils import find_wav_files
-
+#from dfadetect.lfcc import LFCC
+#from dfadetect.utils import find_wav_files
+.
 LOGGER = logging.getLogger(__name__)
 
 

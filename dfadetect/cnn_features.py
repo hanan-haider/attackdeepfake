cf915614c2ed08cf8ee997bbadcce3d7ff1e585d
Hanan Haider <137984110+hanan-haider@users.noreply.github.com>
2026-04-20 11:52:12 +0500

    .

---
+++ dfadetect/cnn_features.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass, field
+from typing import List
+
+import torch
+import torchaudio
+
+
+@dataclass
+class CNNFeaturesSetting:
+    frontend_algorithm: List[str]= field(default_factory=lambda: ["mfcc"])
+    use_spectrogram: bool = True

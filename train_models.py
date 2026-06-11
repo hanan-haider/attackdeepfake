0f21992a79c8039d0e90f36d8ee9fa0c36a35f3d
Hanan Haider <137984110+hanan-haider@users.noreply.github.com>
2026-06-11 13:05:42 +0500

    .

--- train_models.py
+++ train_models.py
@@ -229,7 +229,10 @@ def parse_args():
     parser.add_argument(
         "--verbose", "-v", help="Display debug information?", action="store_true")
 
- 
+    # GMM arguments
+    parser.add_argument(
+        "--use_gmm", help="[GMM] Use to train GMM, otherwise - NNs", action="store_true"
+    )
 
     default_k = 128
     parser.add_argument(

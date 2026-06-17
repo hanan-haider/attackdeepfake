c3a745a8d8bf286823fd8e1971f587f646516e79
Hanan Haider <137984110+hanan-haider@users.noreply.github.com>
2026-06-17 11:20:10 +0500

    .

--- train_models.py
+++ train_models.py
@@ -77,8 +77,8 @@ def train_nn(
 
     for fold in range(folds_number):
         data_train = AttackAgnosticDataset(
-            #asvspoof_path=datasets_paths[0],
-            #wavefake_path=datasets_paths[1],
+            asvspoof_path=datasets_paths[0],
+            wavefake_path=datasets_paths[1],
             fakeavceleb_path=datasets_paths[2],
             fold_num=fold,
             fold_subset="train",
@@ -88,8 +88,8 @@ def train_nn(
 
         data_test = AttackAgnosticDataset(
             
-            #asvspoof_path=datasets_paths[0],
-            #wavefake_path=datasets_paths[1],
+            asvspoof_path=datasets_paths[0],
+            wavefake_path=datasets_paths[1],
             fakeavceleb_path=datasets_paths[2],
             fold_num=fold,
             fold_subset="test",
